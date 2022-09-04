9db6f9468157df83ca474d58df8f7fb57f433d53
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2022-09-04 12:27:08 -0300

    Update Perfil.java
    Finalização do exercício Perfil.java

--- 4-POO-PROGRAMACAO-ORIENTADA-A-OBJETOS/Aula 3/Perfil.java
+++ 4-POO-PROGRAMACAO-ORIENTADA-A-OBJETOS/Aula 3/Perfil.java
@@ -1,18 +1,34 @@
-import.java.util.Scanner
+import java.util.Scanner;
 
-public class Perfil(){
-   public static void main(String[] args) {
-      String data;
-      
-      Scanner input = new Scanner (System.in);
-      System.out.print("Digite a data no formao DD/MM/YYYY");      
-      data = input.next();
-      
-      int soma1 = Integer.parseInt(data.substring(0,4)) + Integer.parseInt(data.substring(4,8));
-      int soma2 = Integer.parseInt(Integer(soma).toString().substring(0,2)) + Integer.parseInt(Integer(soma).toString().substring(2,4));
-    
-      System.out.print("Dia/M�s" + soma2);
-      //System.out.print("Ano" + ano);    
-      
-   }
+public class Perfil {
+    public static void main(String[] args) {
+        String data;
+        
+        Scanner input = new Scanner (System.in);
+        System.out.print("Digite a data no formato DD/MM/YYYY: ");      
+        data = input.next();
+        input.close();
+        
+        int soma1 = Integer.parseInt(data.substring(0,4)) + Integer.parseInt(data.substring(4,8));
+        int soma2 = Integer.parseInt(Integer.toString(soma1).substring(0,2)) + Integer.parseInt(Integer.toString(soma1).substring(2,4));
+        int res = soma2 % 5;
+
+        switch(res){
+            case 0:
+            System.out.println("Seu perfil é tímido");
+            break;
+            case 1:
+            System.out.println("Seu perfil é sonhador");
+            break;
+            case 2:
+            System.out.println("Seu perfil é paquerador");
+            break;
+            case 3:
+            System.out.println("Seu perfil é atraente");
+            break;
+            case 4:
+            System.out.println("Seu perfil é irresistível");
+            break;
+        } 
+    }    
 }
